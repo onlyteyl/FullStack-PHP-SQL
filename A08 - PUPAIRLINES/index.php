1b92ba7e0afe75a78f6aad9027556a6999c282fd
teylstack <allenendaya43@gmail.com>
2024-12-14 00:15:37 +0800

    ADD ICON

--- A08 - PUPAIRLINES/index.php
+++ A08 - PUPAIRLINES/index.php
@@ -40,6 +40,7 @@ $airlineResult = executeQuery($airlinequery);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="style.css" rel="stylesheet">
+    <link href="PUP.png" rel="icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
